1b6c9828dc0e1ea152e6b4bb3e911ae84850d675
ArivazhaganPandiyan <arivutamizhan@icloud.com>
2024-03-11 14:17:08 +0530

    first commit

---
+++ src/helpers/API.js
@@ -0,0 +1,5 @@
+// Server side URL
+export const API = 'https://reset-password-server-x5p7.onrender.com/' ;
+
+// client side URL for new password
+export const AppLink = 'https://65eec63d0045e620436f0c4b--mellow-florentine-f8de1a.netlify.app/' ;
